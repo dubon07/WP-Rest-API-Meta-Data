a80ccf391b719e0a998b72f8dba72703a2e16174
Carlos <dubon07@gmail.com>
2017-09-11 11:27:43 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+/*
+Plugin Name: PGI REST API Meta Data 
+Plugin URI:  
+Description: Playground Ideas | Add Meta Data in REST API Response
+Version:     1.0
+Author:      Carlos Mendoza
+Author URI:  https://www.mendozalearninghub.com
+*/
+
+/*
+    Working with registered meta in the WP REST API
+*/    
+// The object type. For custom post types, this is 'post';
+// for custom comment types, this is 'comment'. For user meta,
+// this is 'user'.
+$object_type = 'user';
+
+
+// Please contact me for this code!!! dubon07@gmail.com ! Thanks!
+
